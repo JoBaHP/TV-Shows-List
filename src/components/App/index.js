29a5ea8801787ff8b5863e359cb0281d009bc29e
Jovan R <jocoraos@yahoo.com>
2021-04-16 13:44:53 +0200

    refactoring

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,23 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
-import Intro from "../Intro";
 import Series from "../../containers/Series";
 import "whatwg-fetch";
 
 class App extends Component {
-  state = {
-    series: [],
-  };
-  componentDidMount() {
-    fetch("http://api.tvmaze.com/search/shows?q=girls").then((response) =>
-      response.json().then((json) => this.setState({ series: json }))
-    );
-  }
   render() {
     return (
       <div className="App">
-        <header className="App-header"></header>
-        <Intro message="Here you can find all your TV Shows!" />
+        <header className="App-header">
+          <h1 className="App-title">TV SHOWS LIST</h1>
+        </header>
 
         <Series />
       </div>
